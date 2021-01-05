181168d4991de2b54f5a9bd5e839ac7e647a3601
mvdzee <31038418+mvdzee@users.noreply.github.com>
2021-01-05 09:20:38 +0000

    Update TD_windows.R

--- scripts/TD_windows.R
+++ scripts/TD_windows.R
@@ -1,13 +1,14 @@
+# This R script processes the raw, concatenated Tajima's D file created by PopGenome
+
 library(data.table)
 library(stringr)
 library(VennDiagram)
 
-### This script processes the raw neutrality stats data files from angsd
+# Read in the concatenated file and reorder the table
 td <- read.table("data/popgenome/FIBR_TD.txt", row.names = NULL, header = TRUE)
 td$window_start<-td$window_start+1
 td['window_id']<-paste0(td$chrom, ":",td$window_start,"-",td$window_end)
 td2<-td[,c(1:4,11,5:10)]
-
 td2<-na.omit(td2)
 
 ### create empty data frame
@@ -15,7 +16,7 @@ td_rbind<-data.frame(matrix(ncol=7))
 colnames(td_rbind)<-c(colnames(td2)[c(1:5)],"TD","comp")
 
 ### loop to re-format & fill the empty dataframe. Now has a column pi and comp (=comparison)
-## CHANGE 5:8 to 5:9 when UL is done!
+## Change '6:11' to include the appropriate columns from your data
 for (i in 6:11){
   pi_tmp<-td2[,c(1:5,i)]
   pi_tmp$comp<-rep(colnames(td2)[i],length(pi_tmp$chrom))
